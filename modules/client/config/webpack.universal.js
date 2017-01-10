69c8cbe1639ec58a1e9963dd102c01465df0fa3e
Dmitrij Kuba <dimakuba124@gmail.com>
2017-01-10 19:09:09 +0200

    update .travis.yml

--- modules/client/config/webpack.universal.js
+++ modules/client/config/webpack.universal.js
@@ -1,12 +1,5 @@
-var webpack = require('webpack');
-var path = require('path');
-var clone = require('js.clone');
-var webpackMerge = require('webpack-merge');
-
-var clientConfig = require('./webpack.client.js');
-var serverConfig = require('./webpack.server.js');
-
-
+const clientConfig = require('./webpack.client.js');
+const serverConfig = require('./webpack.server.js');
 
 module.exports = [
   // Client
